291cc2a0f5257a641125420a2ff6bf352bdd77ac
RamanKSingh <146735890+RamanKSingh@users.noreply.github.com>
2023-10-02 18:36:17 +0530

    Solved assignment files are added

---
+++ First Program, Variables and reading IO/Q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int length, breadth;
+    cout << "Enter Length:";
+    cin >> length;
+    cout << "Enter Breadth:";
+    cin >> breadth;
+    cout << "Area is:" << length * breadth;
+
+    return 0;
+}
---
+++ First Program, Variables and reading IO/Q4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter number:";
+    cin >> number;
+    cout << "Cube is:" << number * number * number;
+
+    return 0;
+}
